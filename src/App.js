c48e1d6cbf29e0d6b4c04bffdd6e2d44e0350987
Allen Cheslik <allencheslik0@gmail.com>
2022-01-26 18:58:25 -0600

    removed non working particles

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
-   <Particles className='tsparticles-canvas-el'
-     params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 900,
-              },
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 6,
-                color: "#f9ab00",
-              },
-            },
-          },
-        }}
-   />
    <Navbar />
    <Header />
    </>   
